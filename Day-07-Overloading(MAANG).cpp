3f6f5d9559d42fc10ecc9ff971f1ac827a524182
Rajpatel7878 <rajp111789@gmail.com>
2026-09-09 23:51:34 +0530

    Your message

---
+++ Day-07-Overloading(MAANG).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void print(int x) {
+    cout << "int";
+}
+
+void print(double x) {
+    cout << "double";
+}
+
+void print(string x) {
+    cout << "string";
+}
+
+int main() {
+    print(10);
+    print(10.5);
+    print("Hello");
+}
