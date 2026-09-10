f689066337c65ea83d36c26fdb46d426f95a1676
Rajpatel7878 <rajp111789@gmail.com>
2026-09-10 20:08:21 +0530

    Your message

---
+++ Day-09-Friend Class.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int marks = 90;
+
+    friend class Teacher;
+};
+
+class Teacher {
+public:
+    void display(Student s) {
+        cout << "Marks = " << s.marks << endl;
+    }
+};
+
+int main() {
+
+    Student s;
+    Teacher t;
+
+    t.display(s);
+
+    return 0;
+}
