305dfe47b4a359a38d17210e39b601865e2af474
Rajpatel7878 <rajp111789@gmail.com>
2026-09-15 22:14:55 +0530

    Swap Two Numbers Using Pointers

---
+++ Day-18-Swap Using Pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void swapNumbers(int* a, int* b) {
+
+    int temp = *a;
+
+    *a = *b;
+
+    *b = temp;
+}
+
+int main() {
+
+    int a = 10;
+    int b = 20;
+
+    swapNumbers(&a, &b);
+
+    cout << a << " " << b;
+
+    return 0;
+}
+
+//Output:
+//20 10
